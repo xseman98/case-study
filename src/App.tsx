186efd495bb3928826e6519d19f0d255ce3af86e
Kristian Seman <78860937+xseman98@users.noreply.github.com>
2024-03-06 09:58:31 +0100

    feat: use auth context

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext'
 import Routes from './router/Routes'
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <div className="App">
-        <Routes />
-      </div>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <div className="App bg-gray-100">
+          <Routes />
+        </div>
+      </Router>
+    </AuthProvider>
   )
 }
 
