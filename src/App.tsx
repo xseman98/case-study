8a21df5ecc85d8e31adf1875af351421275d657a
Kristian Seman <78860937+xseman98@users.noreply.github.com>
2024-03-06 10:29:53 +0100

    feat: use posts context

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext'
+import { PostsProvider } from './context/PostsContext'
 import Routes from './router/Routes'
 
 const App: React.FC = () => {
   return (
     <AuthProvider>
-      <Router>
-        <div className="App bg-gray-100">
-          <Routes />
-        </div>
-      </Router>
+      <PostsProvider>
+        <Router>
+          <div className="App bg-gray-100">
+            <Routes />
+          </div>
+        </Router>
+      </PostsProvider>
     </AuthProvider>
   )
 }
